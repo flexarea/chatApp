8eac4fcfe52ff13d3edfb848f6d233245d76f1e3
esdras <esdrasntuyenabo40@gmail.com>
2024-07-24 12:55:43 +0200

    configuring form for login

--- src/app/(site)/components/AuthForm.tsx
+++ src/app/(site)/components/AuthForm.tsx
@@ -1,5 +1,49 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { CgPassword } from "react-icons/cg";
+import { TbVariableMinus } from "react-icons/tb";
+
+type Variant = "LOGIN" | "REGISTER";
+
 const AuthForm = () => {
-  return <div>Auth Form!</div>;
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  //submit function
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === "REGISTER") {
+      //Axios Register
+    }
+    if (variant === "LOGIN") {
+      //NextAuth signin
+    }
+  };
+
+  return <div>Auth Form2!</div>;
 };
 
 export default AuthForm;
