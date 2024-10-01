d51c9597bd965b82e5a07a5009c4618052689172
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2024-10-01 12:20:02 -0400

    fixed neovim and install a bunch of irrelevant packages

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,8 +3,7 @@ import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
-    <div
-      className="
+    <div className="
     flex
     min-h-full
     flex-col
@@ -13,8 +12,7 @@ export default function Home() {
     sm:px-6
     lg:px-8
     bg-gray-100
-    "
-    >
+    ">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
           alt="Logo"
@@ -23,16 +21,14 @@ export default function Home() {
           className="mx-auto w-auto"
           src="/images/logo.png"
         />
-        <h2
-          className="
+        <h2 className="
         mt-6
         text-center
         text-3xl
         font-bold
         tracking-tight
         text-gray-900
-        "
-        >
+        ">
           Sign in to your account
         </h2>
       </div>
